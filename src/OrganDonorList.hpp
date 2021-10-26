f7d41e115821a23fd4f995d5c1fac4bf982e4e89
lekevin <lekevvin@gmail.com>
2021-10-26 15:05:07 -0500

    Update OrganDonorList.hpp

--- src/OrganDonorList.hpp
+++ src/OrganDonorList.hpp
@@ -2,14 +2,28 @@
  * OrganDonorList.hpp
  *
  *  Created on: Oct 19, 2021
- *      Author: Trong
+ *      Author: Kevin Le
  */
+
+#include <string>
+
+using std::string;
 
 #ifndef ORGANDONORLIST_HPP_
 #define ORGANDONORLIST_HPP_
 
+struct OrganDonorList {
 
+	string organ;
+    int quantity;
 
+	OrganDonorList():
+    organ(""), quantity(){};
+
+	OrganDonorList(string o, int q):
+    organ(o), quantity(q){};
+
+};
 
 
 #endif /* ORGANDONORLIST_HPP_ */
